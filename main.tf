77774b7c6828522f56455a1ec5a3b27835d78afc
Daniel Sol <dannyboy_sol@hotmail.com>
2023-09-07 10:55:31 -0700

    Update main.tf
    adding var support

--- main.tf
+++ main.tf
@@ -24,8 +24,8 @@ provider "azurerm" {
 # Create storage account for boot diagnostics
 resource "azurerm_storage_account" "my_storage_account" {
   name                     = "diag${random_id.random_id.hex}"
-  location                 = azurerm_resource_group.rg.location
-  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.azure_region
+  resource_group_name      = var.azure_rg
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
