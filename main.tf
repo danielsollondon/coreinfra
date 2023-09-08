d8d241bb46e805047d8a64656eb0def50a3f3d28
Daniel Sol <dannyboy_sol@hotmail.com>
2023-09-07 17:47:02 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "azurerm_kubernetes_cluster" "aks001" {
   location            = "westus2"
   resource_group_name = "prod-clu-grp01"
   dns_prefix          = "aks001-k8s"
-  azure_policy_enabled   = true
+  
 
   default_node_pool {
     name       = "default"
@@ -39,6 +39,7 @@ resource "azurerm_kubernetes_cluster" "aks001" {
   }
 
   role_based_access_control_enabled = true
+  azure_policy_enabled = true
 
 
 }
