1f108713412b3b089f34ce3ad6c607a139571c5e
Daniel Sol <dannyboy_sol@hotmail.com>
2023-09-08 16:28:30 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "azurerm_kubernetes_cluster" "aks001" {
 
   default_node_pool {
     name       = "default"
-    node_count = 3
+    node_count = 4
     vm_size    = "Standard_DS2_v2"
   }
 
