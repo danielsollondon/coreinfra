41cf95b9101f39c4b1f164b3b5e43392863530a3
Daniel Sol <dannyboy_sol@hotmail.com>
2023-09-07 18:18:42 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "azurerm_kubernetes_cluster" "aks001" {
 
   lifecycle {
     ignore_changes = [
-      default_node_pool[0].node_count,
+      default_node_pool[0].node_count, tags
     ]
   }
 
