be63b8bb7c2f8aaf72bcfcceabfc292da08ea517
Dan Sol <dansol@DansMac01.local>
2023-09-13 18:03:03 -0700

    adding more res templates

--- templates/AKS/2_Azure_AKS_Cluster.tf
+++ templates/AKS/2_Azure_AKS_Cluster.tf
@@ -8,7 +8,7 @@ resource "azurerm_kubernetes_cluster" "CLUSTERNAME" {
 
   default_node_pool {
     name       = "default"
-    node_count = 3
+    node_count = 2
     vm_size    = "Standard_DS2_v2"
   }
 
